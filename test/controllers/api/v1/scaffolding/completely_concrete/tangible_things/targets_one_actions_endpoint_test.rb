f0c2499285deabd809bfde91ab3abdf1badcff93
Andrew Culver <andrew.culver@gmail.com>
2022-01-18 21:31:03 -0800

    Regenerating scaffolding for targets one action.
    (transferred from bullet-train-co/bullet-train-tailwind-css@c7a668a4b1093ed83ae50589c77434b4409e1ae9)

--- test/controllers/api/v1/scaffolding/completely_concrete/tangible_things/targets_one_actions_endpoint_test.rb
+++ test/controllers/api/v1/scaffolding/completely_concrete/tangible_things/targets_one_actions_endpoint_test.rb
@@ -4,121 +4,121 @@ require "controllers/api/test"
 class Api::V1::Scaffolding::CompletelyConcrete::TangibleThings::TargetsOneActionsEndpointTest < Api::Test
   include Devise::Test::IntegrationHelpers
 
-    def setup
-      # See `test/controllers/api/test.rb` for common set up for API tests.
-      super
+  def setup
+    # See `test/controllers/api/test.rb` for common set up for API tests.
+    super
 
-      @absolutely_abstract_creative_concept = create(:scaffolding_absolutely_abstract_creative_concept, team: @team)
-@tangible_thing = create(:scaffolding_completely_concrete_tangible_thing, tangible_thing: @tangible_thing)
-@targets_one_action = create(:scaffolding_completely_concrete_tangible_things_targets_one_action, targets_one_action: @targets_one_action)
-      @other_targets_one_actions = create_list(:scaffolding_completely_concrete_tangible_things_targets_one_action, 3)
-    end
+    @absolutely_abstract_creative_concept = create(:scaffolding_absolutely_abstract_creative_concept, team: @team)
+    @tangible_thing = create(:scaffolding_completely_concrete_tangible_thing, tangible_thing: @tangible_thing)
+    @targets_one_action = create(:scaffolding_completely_concrete_tangible_things_targets_one_action, targets_one_action: @targets_one_action)
+    @other_targets_one_actions = create_list(:scaffolding_completely_concrete_tangible_things_targets_one_action, 3)
+  end
 
-    # This assertion is written in such a way that new attributes won't cause the tests to start failing, but removing
-    # data we were previously providing to users _will_ break the test suite.
-    def assert_proper_object_serialization(targets_one_action_data)
-      # Fetch the targets_one_action in question and prepare to compare it's attributes.
-      targets_one_action = Scaffolding::CompletelyConcrete::TangibleThings::TargetsOneAction.find(targets_one_action_data["id"])
+  # This assertion is written in such a way that new attributes won't cause the tests to start failing, but removing
+  # data we were previously providing to users _will_ break the test suite.
+  def assert_proper_object_serialization(targets_one_action_data)
+    # Fetch the targets_one_action in question and prepare to compare it's attributes.
+    targets_one_action = Scaffolding::CompletelyConcrete::TangibleThings::TargetsOneAction.find(targets_one_action_data["id"])
 
-      assert_equal targets_one_action_data['keep_receipt'], targets_one_action.keep_receipt
-      assert_equal targets_one_action_data['target_count'], targets_one_action.target_count
-      assert_equal targets_one_action_data['performed_count'], targets_one_action.performed_count
-      assert_equal targets_one_action_data['created_by'], targets_one_action.created_by
-      assert_equal targets_one_action_data['approved_by'], targets_one_action.approved_by
-      assert_equal DateTime.parse(targets_one_action_data['scheduled_for']), targets_one_action.scheduled_for
-      assert_equal DateTime.parse(targets_one_action_data['started_at']), targets_one_action.started_at
-      assert_equal DateTime.parse(targets_one_action_data['completed_at']), targets_one_action.completed_at
-      assert_equal targets_one_action_data['delay'], targets_one_action.delay
-      # 🚅 super scaffolding will insert new fields above this line.
+    assert_equal targets_one_action_data["keep_receipt"], targets_one_action.keep_receipt
+    assert_equal targets_one_action_data["target_count"], targets_one_action.target_count
+    assert_equal targets_one_action_data["performed_count"], targets_one_action.performed_count
+    assert_equal targets_one_action_data["created_by"], targets_one_action.created_by
+    assert_equal targets_one_action_data["approved_by"], targets_one_action.approved_by
+    assert_equal DateTime.parse(targets_one_action_data["scheduled_for"]), targets_one_action.scheduled_for
+    assert_equal DateTime.parse(targets_one_action_data["started_at"]), targets_one_action.started_at
+    assert_equal DateTime.parse(targets_one_action_data["completed_at"]), targets_one_action.completed_at
+    assert_equal targets_one_action_data["delay"], targets_one_action.delay
+    # 🚅 super scaffolding will insert new fields above this line.
 
-      assert_equal targets_one_action_data["tangible_thing_id"], targets_one_action.tangible_thing_id
-    end
+    assert_equal targets_one_action_data["tangible_thing_id"], targets_one_action.tangible_thing_id
+  end
 
-    test "index" do
-      # Fetch and ensure nothing is seriously broken.
-      get "/api/v1/scaffolding/completely_concrete/tangible_things/#{@tangible_thing.id}/targets_one_actions", params: {access_token: access_token}
+  test "index" do
+    # Fetch and ensure nothing is seriously broken.
+    get "/api/v1/scaffolding/completely_concrete/tangible_things/#{@tangible_thing.id}/targets_one_actions", params: {access_token: access_token}
+    assert_response :success
+
+    # Make sure it's returning our resources.
+    targets_one_action_ids_returned = response.parsed_body.dig("data").map { |targets_one_action| targets_one_action.dig("attributes", "id") }
+    assert_includes(targets_one_action_ids_returned, @targets_one_action.id)
+
+    # But not returning other people's resources.
+    assert_not_includes(targets_one_action_ids_returned, @other_targets_one_actions[0].id)
+
+    # And that the object structure is correct.
+    assert_proper_object_serialization response.parsed_body.dig("data").first.dig("attributes")
+  end
+
+  test "show" do
+    # Fetch and ensure nothing is seriously broken.
+    get "/api/v1/scaffolding/completely_concrete/tangible_things/targets_one_actions/#{@targets_one_action.id}", params: {access_token: access_token}
+    assert_response :success
+
+    # Ensure all the required data is returned properly.
+    assert_proper_object_serialization response.parsed_body.dig("data", "attributes")
+
+    # Also ensure we can't do that same action as another user.
+    get "/api/v1/scaffolding/completely_concrete/tangible_things/targets_one_actions/#{@targets_one_action.id}", params: {access_token: another_access_token}
+    assert_response_specific_not_found
+  end
+
+  test "create" do
+    # Use the serializer to generate a payload, but strip some attributes out.
+    targets_one_action_data = Api::V1::Scaffolding::CompletelyConcrete::TangibleThings::TargetsOneActionSerializer.new(build(:scaffolding_completely_concrete_tangible_things_targets_one_action, tangible_thing: nil)).serializable_hash.dig(:data, :attributes)
+    targets_one_action_data.except!(:id, :tangible_thing_id, :created_at, :updated_at)
+
+    post "/api/v1/scaffolding/completely_concrete/tangible_things/#{@tangible_thing.id}/targets_one_actions",
+      params: targets_one_action_data.merge({access_token: access_token})
+
+    assert_response :success
+
+    # Ensure all the required data is returned properly.
+    assert_proper_object_serialization response.parsed_body.dig("data", "attributes")
+
+    # Also ensure we can't do that same action as another user.
+    post "/api/v1/scaffolding/completely_concrete/tangible_things/#{@tangible_thing.id}/targets_one_actions",
+      params: targets_one_action_data.merge({access_token: another_access_token})
+    # TODO Why is this returning forbidden instead of the specific "Not Found" we get everywhere else?
+    assert_response :forbidden
+  end
+
+  test "update" do
+    # Post an attribute update ensure nothing is seriously broken.
+    put "/api/v1/scaffolding/completely_concrete/tangible_things/targets_one_actions/#{@targets_one_action.id}", params: {
+      access_token: access_token,
+      target_count: "Alternative String Value",
+      performed_count: "Alternative String Value",
+      delay: "Alternative String Value",
+      # 🚅 super scaffolding will also insert new fields above this line.
+    }
+
+    assert_response :success
+
+    # Ensure all the required data is returned properly.
+    assert_proper_object_serialization response.parsed_body.dig("data", "attributes")
+
+    # But we have to manually assert the value was properly updated.
+    @targets_one_action.reload
+    assert_equal @targets_one_action.target_count, "Alternative String Value"
+    assert_equal @targets_one_action.performed_count, "Alternative String Value"
+    assert_equal @targets_one_action.delay, "Alternative String Value"
+    # 🚅 super scaffolding will additionally insert new fields above this line.
+
+    # Also ensure we can't do that same action as another user.
+    put "/api/v1/scaffolding/completely_concrete/tangible_things/targets_one_actions/#{@targets_one_action.id}", params: {access_token: another_access_token}
+    assert_response_specific_not_found
+  end
+
+  test "destroy" do
+    # Delete and ensure it actually went away.
+    assert_difference("Scaffolding::CompletelyConcrete::TangibleThings::TargetsOneAction.count", -1) do
+      delete "/api/v1/scaffolding/completely_concrete/tangible_things/targets_one_actions/#{@targets_one_action.id}", params: {access_token: access_token}
       assert_response :success
-
-      # Make sure it's returning our resources.
-      targets_one_action_ids_returned = response.parsed_body.dig("data").map { |targets_one_action| targets_one_action.dig("attributes", "id") }
-      assert_includes(targets_one_action_ids_returned, @targets_one_action.id)
-
-      # But not returning other people's resources.
-      assert_not_includes(targets_one_action_ids_returned, @other_targets_one_actions[0].id)
-
-      # And that the object structure is correct.
-      assert_proper_object_serialization response.parsed_body.dig("data").first.dig("attributes")
     end
 
-    test "show" do
-      # Fetch and ensure nothing is seriously broken.
-      get "/api/v1/scaffolding/completely_concrete/tangible_things/targets_one_actions/#{@targets_one_action.id}", params: {access_token: access_token}
-      assert_response :success
-
-      # Ensure all the required data is returned properly.
-      assert_proper_object_serialization response.parsed_body.dig("data", "attributes")
-
-      # Also ensure we can't do that same action as another user.
-      get "/api/v1/scaffolding/completely_concrete/tangible_things/targets_one_actions/#{@targets_one_action.id}", params: {access_token: another_access_token}
-      assert_response_specific_not_found
-    end
-
-    test "create" do
-      # Use the serializer to generate a payload, but strip some attributes out.
-      targets_one_action_data = Api::V1::Scaffolding::CompletelyConcrete::TangibleThings::TargetsOneActionSerializer.new(build(:scaffolding_completely_concrete_tangible_things_targets_one_action, tangible_thing: nil)).serializable_hash.dig(:data, :attributes)
-      targets_one_action_data.except!(:id, :tangible_thing_id, :created_at, :updated_at)
-
-      post "/api/v1/scaffolding/completely_concrete/tangible_things/#{@tangible_thing.id}/targets_one_actions",
-        params: targets_one_action_data.merge({access_token: access_token})
-
-      assert_response :success
-
-      # Ensure all the required data is returned properly.
-      assert_proper_object_serialization response.parsed_body.dig("data", "attributes")
-
-      # Also ensure we can't do that same action as another user.
-      post "/api/v1/scaffolding/completely_concrete/tangible_things/#{@tangible_thing.id}/targets_one_actions",
-        params: targets_one_action_data.merge({access_token: another_access_token})
-      # TODO Why is this returning forbidden instead of the specific "Not Found" we get everywhere else?
-      assert_response :forbidden
-    end
-
-    test "update" do
-      # Post an attribute update ensure nothing is seriously broken.
-      put "/api/v1/scaffolding/completely_concrete/tangible_things/targets_one_actions/#{@targets_one_action.id}", params: {
-        access_token: access_token,
-        target_count: 'Alternative String Value',
-        performed_count: 'Alternative String Value',
-        delay: 'Alternative String Value',
-        # 🚅 super scaffolding will also insert new fields above this line.
-      }
-
-      assert_response :success
-
-      # Ensure all the required data is returned properly.
-      assert_proper_object_serialization response.parsed_body.dig("data", "attributes")
-
-      # But we have to manually assert the value was properly updated.
-      @targets_one_action.reload
-      assert_equal @targets_one_action.target_count, 'Alternative String Value'
-      assert_equal @targets_one_action.performed_count, 'Alternative String Value'
-      assert_equal @targets_one_action.delay, 'Alternative String Value'
-      # 🚅 super scaffolding will additionally insert new fields above this line.
-
-      # Also ensure we can't do that same action as another user.
-      put "/api/v1/scaffolding/completely_concrete/tangible_things/targets_one_actions/#{@targets_one_action.id}", params: {access_token: another_access_token}
-      assert_response_specific_not_found
-    end
-
-    test "destroy" do
-      # Delete and ensure it actually went away.
-      assert_difference("Scaffolding::CompletelyConcrete::TangibleThings::TargetsOneAction.count", -1) do
-        delete "/api/v1/scaffolding/completely_concrete/tangible_things/targets_one_actions/#{@targets_one_action.id}", params: {access_token: access_token}
-        assert_response :success
-      end
-
-      # Also ensure we can't do that same action as another user.
-      delete "/api/v1/scaffolding/completely_concrete/tangible_things/targets_one_actions/#{@targets_one_action.id}", params: {access_token: another_access_token}
-      assert_response_specific_not_found
-    end
+    # Also ensure we can't do that same action as another user.
+    delete "/api/v1/scaffolding/completely_concrete/tangible_things/targets_one_actions/#{@targets_one_action.id}", params: {access_token: another_access_token}
+    assert_response_specific_not_found
+  end
 end
