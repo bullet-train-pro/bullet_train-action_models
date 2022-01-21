5acc5633f5ffd8e8abec0a6e220b1abeb974c2ab
Andrew Culver <andrew.culver@gmail.com>
2022-01-21 08:42:50 -0800

    Updating a bunch of stuff.
    (transferred from bullet-train-co/bullet-train-tailwind-css@e09cd9b3494b5a533aaa469f130f22f5d1246a23)

--- lib/scaffolding/action_model_targets_one_parent_scaffolder.rb
+++ lib/scaffolding/action_model_targets_one_parent_scaffolder.rb
@@ -1,9 +1,9 @@
-require_relative "action_model_targets_many_transformer"
+require_relative "action_model_targets_one_parent_transformer"
 
-module ActionModelTargetsManyScaffolder
-  def display_action_model_targets_many_help
+module ActionModelTargetsOneParentScaffolder
+  def display_action_model_targets_one_parent_help
     puts ""
-    puts "🚅  usage: bin/super-scaffold action-model:targets-many <ActionModel> <TargetModel> <ParentModel[s]>"
+    puts "🚅  usage: bin/super-scaffold action-model:targets-one-parent <ActionModel> <TargetModel> <ParentModel[s]>"
     puts ""
     puts "E.g. an Archive action for Projects on a Team:"
     puts "  # You do not need to `rails g model`, we'll create the models!"
@@ -18,26 +18,24 @@ module ActionModelTargetsManyScaffolder
     exit
   end
 
-  def scaffold_action_model_targets_many(args)
+  def scaffold_action_model_targets_one_parent(args)
     action_model, target_model, parent_models = args
     # action_model += "Action" unless action_model.match?(/Action$/)
     parent_models = parent_models.split(",")
     parent_models += ["Team"]
     parent_models = parent_models.map(&:classify).uniq
 
-    transformer = Scaffolding::ActionModelTargetsManyTransformer.new(action_model, target_model, parent_models)
+    transformer = Scaffolding::ActionModelTargetsOneParentTransformer.new(action_model, target_model, parent_models)
 
-    output = `bin/rails g model #{transformer.transform_string("Scaffolding::CompletelyConcrete::TangibleThings::TargetsManyAction")} #{transformer.transform_string("absolutely_abstract_creative_concept")}:references target_all:boolean target_ids:jsonb started_at:datetime completed_at:datetime target_count:integer performed_count:integer scheduled_for:datetime sidekiq_jid:string created_by:references approved_by:references`
+    output = `bin/rails g model #{transformer.transform_string("Scaffolding::CompletelyConcrete::TangibleThings::TargetsOneParentAction")} #{transformer.transform_string("absolutely_abstract_creative_concept")}:references started_at:datetime completed_at:datetime target_count:integer performed_count:integer scheduled_for:datetime sidekiq_jid:string created_by:references approved_by:references`
 
     # if output.include?("conflict") || output.include?("identical")
     #   puts "\n👆 No problem! Looks like you're re-running this Super Scaffolding command. We can work with the model already generated!\n".green
     # end
 
-    migration_file_name = `grep "create_table :#{transformer.transform_string("scaffolding_completely_concrete_tangible_things_targets_many_actions")} do |t|" db/migrate/*`.split(":").first
+    migration_file_name = `grep "create_table :#{transformer.transform_string("scaffolding_completely_concrete_tangible_things_targets_one_parent_actions")} do |t|" db/migrate/*`.split(":").first
 
     legacy_replace_in_file(migration_file_name, "t.references :absolutely_abstract_creative_concept, null: false, foreign_key: true", "t.references :absolutely_abstract_creative_concept, null: false, foreign_key: {to_table: \"scaffolding_absolutely_abstract_creative_concepts\"}")
-    legacy_replace_in_file(migration_file_name, "t.boolean :target_all", "t.boolean :target_all, default: false")
-    legacy_replace_in_file(migration_file_name, "t.jsonb :target_ids", "t.jsonb :target_ids, default: []")
     legacy_replace_in_file(migration_file_name, "t.integer :performed_count", "t.integer :performed_count, default: 0")
 
     created_by_index_name = transformer.transform_string("index_scaffolding_completely_concrete_tangible_things_#{action_model.pluralize.underscore.downcase}_on_created_by_id")
@@ -52,7 +50,7 @@ module ActionModelTargetsManyScaffolder
 
     # If the target model belongs directly to Team, we end up with delegate :team, to :team in the model file so we remove it here.
     # We would need this for a deeply nested resource (I _think_??)
-    legacy_replace_in_file(transformer.transform_string("./app/models/scaffolding/completely_concrete/tangible_things/targets_many_action.rb"), "delegate :team, to: :team", "")
+    legacy_replace_in_file(transformer.transform_string("./app/models/scaffolding/completely_concrete/tangible_things/targets_one_parent_action.rb"), "delegate :team, to: :team", "")
 
     transformer.restart_server
   end
---
+++ lib/scaffolding/action_model_targets_one_parent_transformer.rb
@@ -0,0 +1,145 @@
+require_relative "transformer"
+
+class Scaffolding::ActionModelTargetsOneParentTransformer < Scaffolding::Transformer
+  attr_accessor :action
+
+  def initialize(action, child, parents, cli_options = {})
+    super(child, parents, cli_options)
+    self.action = action
+  end
+
+  def add_locale_helper_export_fix
+    namespaced_locale_export_hook = "# 🚅 super scaffolding will insert the export for the locale view helper here."
+
+    spacer = "  "
+    indentation = spacer * 3
+    namespace_elements = child.underscore.pluralize.split("/")
+    first_element = namespace_elements.shift
+    lines_to_add = [first_element + ":"]
+    namespace_elements.map do |namespace_element|
+      lines_to_add << indentation + namespace_element + ":"
+      indentation += spacer
+    end
+
+    lines_to_add << transform_string("#{indentation}targets_one_parent_actions: *targets_one_parent_actions")
+
+    scaffold_replace_line_in_file("./config/locales/en/scaffolding/completely_concrete/tangible_things/targets_one_parent_actions.en.yml", lines_to_add.join("\n"), namespaced_locale_export_hook)
+  end
+
+  RUBY_NEW_ACTION_MODEL_BUTTONS_PROCESSING_HOOK = "<%# 🚅 super scaffolding will insert new action model buttons above this line. %>"
+  RUBY_NEW_BULK_ACTION_MODEL_BUTTONS_PROCESSING_HOOK = "<%# 🚅 super scaffolding will insert new bulk action model buttons above this line. %>"
+  RUBY_NEW_ACTION_MODEL_INDEX_VIEWS_PROCESSING_HOOK = "<%# 🚅 super scaffolding will insert new action model index views above this line. %>"
+
+  def scaffold_action_model
+    files = [
+      "./app/models/scaffolding/completely_concrete/tangible_things/targets_one_parent_action.rb",
+      "./app/serializers/api/v1/scaffolding/completely_concrete/tangible_things/targets_one_parent_action_serializer.rb",
+      "./app/controllers/api/v1/scaffolding/completely_concrete/tangible_things/targets_one_parent_actions_endpoint.rb",
+      "./app/controllers/account/scaffolding/completely_concrete/tangible_things/targets_one_parent_actions_controller.rb",
+      "./app/views/account/scaffolding/completely_concrete/tangible_things/targets_one_parent_actions",
+      "./test/models/scaffolding/completely_concrete/tangible_things/targets_one_parent_action_test.rb",
+      "./test/factories/scaffolding/completely_concrete/tangible_things/targets_one_parent_actions.rb",
+      "./test/controllers/api/v1/scaffolding/completely_concrete/tangible_things/targets_one_parent_actions_endpoint_test.rb",
+      "./config/locales/en/scaffolding/completely_concrete/tangible_things/targets_one_parent_actions.en.yml",
+    ]
+
+    files.each do |name|
+      if File.directory?(name)
+        scaffold_directory(name)
+      else
+        scaffold_file(name)
+      end
+    end
+
+    add_locale_helper_export_fix
+
+    # Add the action button to the target _index partial
+    target_index_file = "./app/views/account/scaffolding/completely_concrete/tangible_things/_index.html.erb"
+    scaffold_add_line_to_file(
+      target_index_file,
+      "<%= render \"account/scaffolding/completely_concrete/tangible_things/targets_one_parent_actions/new_button_one\", absolutely_abstract_creative_concept: absolutely_abstract_creative_concept, tangible_thing: tangible_thing %>",
+      RUBY_NEW_BULK_ACTION_MODEL_BUTTONS_PROCESSING_HOOK,
+      prepend: true
+    )
+
+    # Add the action index partial to the target _index partial
+    scaffold_add_line_to_file(
+      target_index_file,
+      "<%= render 'account/scaffolding/completely_concrete/tangible_things/targets_one_parent_actions/index', targets_one_parent_actions: context.completely_concrete_tangible_things_targets_one_parent_actions %>",
+      RUBY_NEW_ACTION_MODEL_INDEX_VIEWS_PROCESSING_HOOK,
+      prepend: true
+    )
+
+    # Add the has_many to the parent model (not the target)
+    scaffold_add_line_to_file(
+      "./app/models/scaffolding/absolutely_abstract/creative_concept.rb",
+      "has_many :completely_concrete_tangible_things_targets_one_parent_actions, class_name: \"Scaffolding::CompletelyConcrete::TangibleThings::TargetsOneParentAction\", dependent: :destroy, foreign_key: :absolutely_abstract_creative_concept_id, enable_updates: true, inverse_of: :absolutely_abstract_creative_concept",
+      HAS_MANY_HOOK,
+      prepend: true
+    )
+
+    # Update the ability file
+    add_line_to_file("app/models/ability.rb", transform_string("Scaffolding::CompletelyConcrete::TangibleThings::TargetsOneParentAction,"), "# 🚅 add action models above.", prepend: true)
+    # Update the ability file
+
+    # Add the concern we have to add manually because otherwise it gets transformed.
+    add_line_to_file(transform_string("app/models/scaffolding/completely_concrete/tangible_things/targets_one_parent_action.rb"), "include Actions::TargetsOneParent", "include Actions::SupportsScheduling", prepend: true)
+
+    # Restart the server to pick up the translation files
+    restart_server
+
+    # Update the routes to add the namespace and action routes
+    routes_manipulator = Scaffolding::RoutesFileManipulator.new("config/routes.rb", transform_string("Scaffolding::CompletelyConcrete::TangibleThings::TargetsOneParentAction"), transform_string("Scaffolding::AbsolutelyAbstract::CreativeConcept"))
+    routes_manipulator.apply(["account"])
+    # TODO We need this to also add `post :approve` to the resource block as well. Do we support that already?
+    routes_manipulator.write
+
+    add_additional_step :yellow, "We've generated a new model and migration file for you, so make sure to run `rake db:migrate`."
+
+    additional_steps.each_with_index do |additional_step, index|
+      color, message = additional_step
+      puts ""
+      puts "#{index + 1}. #{message}".send(color)
+    end
+    puts ""
+  end
+
+  def transform_string(string)
+    string = super(string)
+
+    [
+      "Targets One Parent to",
+      "append an emoji to",
+      "TargetsOneParent",
+      "targets_one_parent",
+      "Targets One Parent",
+    ].each do |needle|
+      # TODO There might be more to do here?
+      # What method is this calling?
+      string = string.gsub(needle, encode_double_replacement_fix(replacement_for(needle)))
+    end
+    decode_double_replacement_fix(string)
+  end
+
+  def replacement_for(string)
+    case string
+    # Some weird edge cases we unwittingly introduced in the emoji example.
+    when "Targets One Parent to"
+      # e.g. "Archive"
+      # If someone wants language like "Targets One Parent to", they have to add it manually or name their model that.
+      action.titlecase
+    when "append an emoji to"
+      # e.g. "archive"
+      # If someone wants language like "append an emoji to", they have to add it manually.
+      action.humanize
+    when "TargetsOneParent"
+      action
+    when "targets_one_parent"
+      action.underscore
+    when "Targets One Parent"
+      action.titlecase
+    else
+      "🛑"
+    end
+  end
+end
