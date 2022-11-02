483341e38ae4effa50aefc7420bb3861d53edd04
Gabriel Zayas <g-zayas@hotmail.com>
2022-11-02 13:54:29 +0900

    Super Scaffold API views and controller test for targets-many

--- lib/scaffolding/action_model_transformer.rb
+++ lib/scaffolding/action_model_transformer.rb
@@ -169,8 +169,10 @@ class Scaffolding::ActionModelTransformer < Scaffolding::Transformer
       "./app/controllers/account/scaffolding/completely_concrete/tangible_things/#{targets_n}_actions_controller.rb",
       "./app/controllers/api/v1/scaffolding/completely_concrete/tangible_things/#{targets_n}_actions_controller.rb",
       "./app/views/account/scaffolding/completely_concrete/tangible_things/#{targets_n}_actions",
+      "./app/views/api/v1/scaffolding/completely_concrete/tangible_things/#{targets_n}_actions",
       "./test/models/scaffolding/completely_concrete/tangible_things/#{targets_n}_action_test.rb",
       "./test/factories/scaffolding/completely_concrete/tangible_things/#{targets_n}_actions.rb",
+      "./test/controllers/api/v1/scaffolding/completely_concrete/tangible_things/#{targets_n}_actions_controller_test.rb",
       "./config/locales/en/scaffolding/completely_concrete/tangible_things/#{targets_n}_actions.en.yml",
     ]
 
