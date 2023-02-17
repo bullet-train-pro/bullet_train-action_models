bfb300b807aa34398562c163688905f4041e3e1d
Pete Keen <pete.keen@clickfunnels.com>
2023-02-17 13:34:22 -0500

    Bail out early if ProcessesAsync discovers it has been included in the wrong order

--- app/models/concerns/actions/processes_async.rb
+++ app/models/concerns/actions/processes_async.rb
@@ -1,6 +1,13 @@
 module Actions::ProcessesAsync
   extend ActiveSupport::Concern
 
+  included do
+    unless @_actions_base_included
+      warn "ERROR: In #{self.name}: Actions::ProcessesAsync must be included _after_ Actions::Base, usually via Actions::TargetsOne or Actions::TargetsMany"
+      exit 254
+    end
+  end
+
   def dispatch
     self.sidekiq_jid = Actions::BackgroundActionWorker.perform_async(self.class.name, id)
     save
