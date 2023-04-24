ba1353ee711f2333b32007e165d016906af57e38
Nick Phillips <nick.phillips@clickfunnels.com>
2023-04-24 15:59:06 -0500

    force ordering

--- app/models/concerns/actions/targets_many.rb
+++ app/models/concerns/actions/targets_many.rb
@@ -33,7 +33,7 @@ module Actions::TargetsMany
   end
 
   def remaining_targets
-    targeted.where("id > ?", last_completed_id).where.not(id: failed_ids).order(:id)
+    targeted.where("id > ?", last_completed_id).where.not(id: failed_ids).reorder(:id)
   end
 
   def health_check_frequency
